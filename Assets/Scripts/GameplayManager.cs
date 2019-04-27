a945c7cc9e7ac1fe69f157edbbae1cb344f165b2
Moro <marcinmoroz@gmail.com>
2019-04-27 16:01:28 +0200

    Poczatek

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Collections;
+
+public class GameplayManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {        
+        GameObject board = Instantiate(Resources.Load("Board"), position, rotation);    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
